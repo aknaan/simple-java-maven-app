2553cdc2274087f54261cf3babf678a0681cb6c5
amir <aknaan1510@gmail.com>
2024-05-05 18:26:35 +0300

    add a new headline #patch

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! let's test versioning!";
+    private static final String MESSAGE = "Hello World! again!";
 
     public App() {}
 
